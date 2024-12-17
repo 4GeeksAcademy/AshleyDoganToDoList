7bb2a4775786ebb2961a79127ea48ccbac5d2fa2
Ashley Dogan <a.a.rappette@gmail.com>
2024-12-17 01:45:23 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [taskInput,setTaskInput]=useState("")
+	const [toDo,setToDo]=useState([])
+	
+	const handleSubmit = (e) => {
+		e.preventDefault()
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		if(taskInput.length>0) {
+			setToDo([
+				{
+					label: taskInput,
+					is_done: false,
+				},
+				...toDo
+			]);
+			setTaskInput("")
+		}
+	}
+
+	const deleteToDo = (index) => {
+		setToDo(
+			toDo.toSpliced(index,1)
+		)
+	}
+	return (
+		<div className="container bg-light border justify-content-center">
+			<h1 className="text-center mt-5">To Do List</h1>
+			<form onSubmit={handleSubmit}>
+				<div className="row">
+					<h4>{taskInput}</h4>
+					<input
+					className="new-todo"
+					placeholder="What needs to be done?"
+					value={taskInput}
+					onChange={(e) => setTaskInput(e.target.value)}
+					/>
+
+				</div>
+			</form>
+			<div className= "row">
+				{toDo.map((item,index)=>(
+					<>
+						<h4 key={index}>
+							{item.label}
+						</h4><button type="button" onClick={deleteToDo} className="btn btn-danger">Done</button>
+					</>
+				))}
+			</div>
 		</div>
 	);
 };
