4364e4badf1790b7ee7786bc9d45081b53f6f182
Ashley Dogan <a.a.rappette@gmail.com>
2024-12-18 03:32:44 +0000

    second commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,59 +1,73 @@
 import React, { useState } from "react";
-
+import "../../styles/home.css";
 //include images into your bundle
-
 
 //create your first component
 const Home = () => {
-	const [taskInput,setTaskInput]=useState("")
-	const [toDo,setToDo]=useState([])
-	
-	const handleSubmit = (e) => {
-		e.preventDefault()
+  const [taskInput, setTaskInput] = useState("");
+  const [toDo, setToDo] = useState([]);
 
-		if(taskInput.length>0) {
-			setToDo([
-				{
-					label: taskInput,
-					is_done: false,
-				},
-				...toDo
-			]);
-			setTaskInput("")
-		}
-	}
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-	const deleteToDo = (index) => {
-		setToDo(
-			toDo.toSpliced(index,1)
-		)
-	}
-	return (
-		<div className="container bg-light border justify-content-center">
-			<h1 className="text-center mt-5">To Do List</h1>
-			<form onSubmit={handleSubmit}>
-				<div className="row">
-					<h4>{taskInput}</h4>
-					<input
-					className="new-todo"
-					placeholder="What needs to be done?"
-					value={taskInput}
-					onChange={(e) => setTaskInput(e.target.value)}
-					/>
+    if (taskInput.length > 0) {
+      setToDo([
+        {
+          label: taskInput,
+          is_done: false,
+        },
+        ...toDo,
+      ]);
+      setTaskInput("");
+    }
+  };
 
-				</div>
-			</form>
-			<div className= "row">
-				{toDo.map((item,index)=>(
-					<>
-						<h4 key={index}>
-							{item.label}
-						</h4><button type="button" onClick={deleteToDo} className="btn btn-danger">Done</button>
-					</>
-				))}
-			</div>
-		</div>
-	);
+  const deleteToDo = (index) => {
+    setToDo(toDo.toSpliced(index, 1));
+  };
+  return (
+    <div className="container justify-content-center">
+      <h1 id="title" className="text-center mt-5">
+        to do
+      </h1>
+      <div className="container bg-light">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              value={taskInput}
+              onChange={(e) => setTaskInput(e.target.value)}
+              class="form-control"
+              type="text"
+            />
+          </div>
+        </form>
+        <div className="row">
+          {toDo.map((item, index) => (
+            <div id="DivForHoverItem">
+              <h4 key={index}>
+                {item.label}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  id="HiddenText"
+                  onClick={deleteToDo}
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-x-lg"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                </svg>
+              </h4>
+            </div>
+          ))}
+        </div>
+        <div className="row">items left</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
